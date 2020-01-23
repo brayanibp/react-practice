7eb3103285d288e6fdd9343c7e960542e83556a4
Brayan Bernal <brayanbernal0710@gmail.com>
2020-01-23 09:40:10 -0400

    Agregando peticiones simuladas a un api

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -4,78 +4,51 @@ import { Link } from 'react-router-dom';
 import './styles/Badges.css';
 import confLogo from '../images/badge-header.svg';
 import BadgesList from '../components/BadgesList';
+import api from '../api';
 
 class Badges extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('1. constructor()');
-
-    this.state = {
-      data: [],
-    };
+  state = {
+    loading: true,
+    error: null,
+    data: undefined,
   }
 
-  componentDidMount() {
-    console.log('3. componentDidMount()');
-
-    this.timeoutId = setTimeout(() => {
-      this.setState({
-        data: [
-          {
-            id: '2de30c42-9deb-40fc-a41f-05e62b5939a7',
-            firstName: 'Freda',
-            lastName: 'Grady',
-            email: 'Leann_Berge@gmail.com',
-            jobTitle: 'Legacy Brand Director',
-            twitter: 'FredaGrady22221-7573',
-            avatarUrl:
-              'https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon',
-          },
-          {
-            id: 'd00d3614-101a-44ca-b6c2-0be075aeed3d',
-            firstName: 'Major',
-            lastName: 'Rodriguez',
-            email: 'Ilene66@hotmail.com',
-            jobTitle: 'Human Research Architect',
-            twitter: 'MajorRodriguez61545',
-            avatarUrl:
-              'https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon',
-          },
-          {
-            id: '63c03386-33a2-4512-9ac1-354ad7bec5e9',
-            firstName: 'Daphney',
-            lastName: 'Torphy',
-            email: 'Ron61@hotmail.com',
-            jobTitle: 'National Markets Officer',
-            twitter: 'DaphneyTorphy96105',
-            avatarUrl:
-              'https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon',
-          },
-        ],
-      });
-    }, 3000);
+  componentDidMount () {
+    this.fetchData();
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log('5. componentDidUpdate()');
-    console.log({
-      prevProps: prevProps,
-      prevState: prevState,
-    });
-
-    console.log({
-      props: this.props,
-      state: this.state,
-    });
-  }
-
-  componentWillUnmount() {
-    console.log('6. componentWillUnmount');
-    clearTimeout(this.timeoutId);
+  fetchData = async ()=>{
+    this.setState({ loading: true, error: null });
+    try {
+      // const data = [];
+      const data = await api.badges.list();
+      this.setState({ loading: false, data: data });
+    } catch(err) {
+      this.setState({ loading: false, error: err });
+    }
   }
 
   render() {
-    console.log('2/4. render()');
+    if (this.state.loading === true) {
+      return 'Loading...'
+    }
+    if (this.state.error) {
+      return (
+        <div className="container">
+          <h3>Something Goes Wrong.</h3>
+          <p>{ this.state.error.message }</p>
+        </div>
+      );
+    }
+    if (this.state.data.length === 0) {
+      return (
+        <div className="container">
+          <h3>No badges were found.</h3>
+          <Link to="/badges/new" className="btn btn-primary">Create new Badge</Link>
+        </div>
+      );
+    }
+
     return (
       <React.Fragment>
         <div className="Badges">
